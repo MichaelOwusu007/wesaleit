a19403ef339cb94fdff7f9a9b4c313372c9f634d
Michael Oppong <michaelopponghero@gmail.com>
2024-08-26 21:01:05 +0000

    I am done with the header

---
+++ script/index.js
@@ -0,0 +1,13 @@
+document.title = 'Blissful Bubbles';
+const hamburger = document.querySelector('.hamburger')
+const navMenu = document.querySelector('ul')
+
+hamburger.addEventListener('click',()=>{
+    hamburger.classList.toggle('active')
+    navMenu.classList.toggle('active')
+})
+
+document.querySelectorAll('a').forEach(n =>n.addEventListener('click',()=>{
+    hamburger.classList.remove('active')
+    navMenu.classList.remove('active')
+}))
